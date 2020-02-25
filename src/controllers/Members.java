1b3dd35221f0891e889b29676dbb8bd9a432f297
Siroyan <1517053@ed.tus.ac.jp>
2020-02-26 08:49:55 +0900

    [update] 会員さんの名前をDBに保存

--- src/controllers/Members.java
+++ src/controllers/Members.java
@@ -1,37 +1,48 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Members {
     /* Index View */
     /* 会員登録ページへ */
-    public void goRegistMemberView(ActionEvent e) throws Exception {
+    @FXML
+    private void goRegistMemberView(ActionEvent e) throws Exception {
         main.Main.changeScene("../views/memberNew.fxml");
     }
 
     /* 会員編集ページへ */
-    public void goEditMemberView(ActionEvent e) throws Exception {
+    @FXML
+    private void goEditMemberView(ActionEvent e) throws Exception {
 
     }
 
     /* 会員削除ページへ */
-    public void goDeleteMemberView(ActionEvent e) throws Exception {
+    @FXML
+    private void goDeleteMemberView(ActionEvent e) throws Exception {
 
     }
 
     /* New View */
+    @FXML
+    public TextField nameField;
     /* 会員を登録 */
-    public void registMember(ActionEvent e){
-        models.Member.regist();
-    }
-
-    /* rootへ移動 */
-    public void returnToMemberIndex(ActionEvent e) throws Exception {
+    @FXML
+    private void registMember(ActionEvent e) throws Exception {
+        models.Member.regist(nameField.getText());
         main.Main.changeScene("../views/memberIndex.fxml");
     }
 
     /* rootへ移動 */
-    public void returnToRoot(ActionEvent e) throws Exception {
+    @FXML
+    private void returnToMemberIndex(ActionEvent e) throws Exception {
+        main.Main.changeScene("../views/memberIndex.fxml");
+    }
+
+    /* rootへ移動 */
+    @FXML
+    private void returnToRoot(ActionEvent e) throws Exception {
         main.Main.changeScene("../views/root.fxml");
     }
 }
