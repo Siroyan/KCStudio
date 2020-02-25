c716dfbfc0144855c485cb9248ac2357efd4cb55
Siroyan <1517053@ed.tus.ac.jp>
2020-02-26 08:46:53 +0900

    [update] 会員の名前をDBへ反映

--- src/controllers/Combinations.java
+++ src/controllers/Combinations.java
@@ -1,19 +1,23 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class Combinations {
     /* 組み合わせを作成 */
-    public void makeCombination(ActionEvent e){
+    @FXML
+    private void makeCombination(ActionEvent e){
 
     }
 
     /* 作成した組み合わせをファイルに出力 */
+    @FXML
     public void exportToFile(ActionEvent e){
 
     }
 
     /* rootへ移動 */
+    @FXML
     public void returnToRoot(ActionEvent e) throws Exception {
         main.Main.changeScene("../views/root.fxml");
     }
